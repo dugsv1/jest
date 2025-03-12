9b76098c0500494ca05df35b9c1e5aced973ad24
Sven Dugojevic <sdugoje@gmail.com>
2025-03-12 16:05:22 -0700

    chore: add jest and babel
    Also added support for jest in eslint thru the eslint
    config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    overrides: [
+      {
+        files: ["tests/**/*"],
+        env: {
+          jest: true,
+        },
+      },
+    ],
+  },
   pluginJs.configs.recommended,
 ];
